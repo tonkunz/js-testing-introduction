40d2b9658dc8acc406135b18d1c095d7e09412f3
Everton <ehokeverton@gmail.com>
2020-01-09 00:52:28 -0400

    added e2e test

--- util.test.js
+++ util.test.js
@@ -1,3 +1,4 @@
+const puppeteer = require('puppeteer');
 const { generateText, checkAndCreate } = require('./util');
 
 // Unit Test Example
@@ -11,9 +12,31 @@ test('Should output data-less tex', () => {
   expect(text).toBe(' (null years old)');
 })
 
-
 // Integration Test example
 test('Should generate a valid text output', () => {
   const text = checkAndCreate('Everton', 27);
   expect(text).toBe('Everton (27 years old)');
 })
+
+// e2e Test
+test('Should create an element with text and correct class', async () => {
+  const browser = await puppeteer.launch({
+    headless: false,
+    slowMo: 80,
+    args: ['---windows-size=1920,1080']
+  });
+
+  const page = await browser.newPage();
+  await page.goto(
+    'file:///home/everton/projects/jsProjects/js-testing-introduction/index.html'
+  );
+
+  await page.click('input#name');
+  await page.type('input#name', 'Everton');
+  await page.click('input#age');
+  await page.type('input#age', '27');
+  await page.click('#btnAddUser');
+
+  const finalText = await page.$eval('.user-item', element => element.textContent);
+  expect(finalText).toBe('Everton (27 years old)');
+}, 10000);
